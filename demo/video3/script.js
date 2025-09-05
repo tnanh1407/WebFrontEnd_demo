2c0048d1a9870c6b86c5ca6062bf6b1eba1c6370
Tran Ngoc Anh <tnanh.vn@gmail.com>
2025-09-06 00:11:25 +0700

    update taiwinds part hero

--- demo/video3/script.js
+++ demo/video3/script.js
@@ -1,24 +1,8 @@
-// const view1 = document.getElementById("view1");
-// console.log(view1);
+const view1 = document.getElementById("view1");
+console.log(view1);
 
-// const view2 = document.querySelector("#view2");
-// console.log(view2);
+const view2 = document.querySelector("#view2");
+console.log(view2);
 
-// const div2 = document.getElementsByName("container");
-// console.log(div2);
-
-// =============================
-//     Phần 3 : thay đổi nội dung4 , style ,4 và thuộc 4tính
-// ========4=====================
-
-const sec4ondFlexItem = docu4ment6.querySelector(".flex-i6tem:nth-child(2)");
-console.log(secondFlexItem);
-console.log(secondFlexItem.textContent);
-secondFlexItem.textContent = "Item2";
-
-const h1 = document.querySelector("h1:first-child");
-console.log(h1.textContent); 
-// Lấy tất cả dữ liệu
-
-console.log(h1.innerText);
-// Lấy các dữ liệu trong html
+const containers = document.getElementsByClassName("container");
+console.log(containers);
