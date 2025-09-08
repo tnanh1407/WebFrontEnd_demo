2d262d0eb14cd04f027ac6cef3e6b32f561c3a6c
Tran Ngoc Anh <tnanh.vn@gmail.com>
2025-09-08 22:46:17 +0700

    update js fetch

---
+++ 28tech/js/bai1/constanst.js
@@ -0,0 +1,1 @@
+export var PI = 3.14;
---
+++ 28tech/js/bai1/main.js
@@ -0,0 +1,8 @@
+import { sum } from "./sum.js";
+import { PI } from "./constanst.js";
+let a = 10;
+let b = 10;
+let c = sum(a, b);
+console.log(c);
+console.log("hello world");
+console.log(PI);
